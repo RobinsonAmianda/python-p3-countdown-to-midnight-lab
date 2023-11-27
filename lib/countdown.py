1dd990c1514338ecefbd8cc83cbc6640fa75d02c
Robinson Amianda <robinsonamianda5@gmail.com>
2023-11-27 13:25:47 +0300

    countdown lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time 
+def countdown(number = 10):
+    while number > 0:
+        print(f"{number} SECONDS(S)!")
+        number = number - 1
+    print("HAPPY NEW YEAR!")
+
+print("```console")    
+def countdown_with_sleep(number = 10):
+    while number > 0:
+        print(f"# => {number} SECONDS(S)!")
+        time.sleep(1)
+        number = number - 1
+    print("# => HAPPY NEW YEAR!")
+    print("```")
+countdown_with_sleep(10)
